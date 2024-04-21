5df4152817cfc7b2e1c37944ed713941f64a6c0c
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-04-20 21:13:01 -0300

    fix: rename directory

--- app/_components/ui/form.tsx
+++ app/_components/ui/form.tsx
@@ -10,7 +10,7 @@ import {
   useFormContext,
 } from "react-hook-form"
 
-import { cn } from "@/_lib/utils"
+import { cn } from "@/app/_lib/utils"
 import { Label } from "@/app/_components/ui/label"
 
 const Form = FormProvider
